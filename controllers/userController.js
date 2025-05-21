5fcf8e2686564b911d8d2947df0b970191d5ddae
Rameez <rameesmohd789@gmail.com>
2025-05-21 18:29:41 +0530

    modified:   controllers/userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -102,7 +102,7 @@ const signup = async (req, res) => {
       .cookie("token", token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: process.env.NODE_ENV === "production" ? "Strict" : "Lax",
+        sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
       })
       .status(200)
